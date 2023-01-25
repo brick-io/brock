fbefdccd9ee53b9d8b06318bf468927f57686c9e
Jibran Fawaid <jibran@onebrick.io>
2023-01-25 13:44:02 +0700

    add metric and smtp sdk

---
+++ sdk/smtp/send.go
@@ -0,0 +1,40 @@
+package smtp
+
+import (
+	"strings"
+
+	"github.com/go-mail/mail"
+)
+
+//nolint:gochecknoglobals
+var cfg SMTPConfiguration
+
+type SMTPConfiguration struct {
+	Host         string
+	Port         int
+	AuthUsername string
+	AuthPassword string
+	Sender       string
+}
+
+func (c SMTPConfiguration) SendEmail(recipient, cc []string, subject, body, attachmentPath string, useHTML bool) error {
+	m := mail.NewMessage()
+	m.SetHeader("From", c.Sender)
+	m.SetHeader("To", strings.Join(recipient, ","))
+	m.SetHeader("Cc", strings.Join(cc, ","))
+	m.SetHeader("Subject", subject)
+
+	if useHTML {
+		m.SetBody("text/html", body)
+	} else {
+		m.SetBody("text/plain", body)
+	}
+
+	if attachmentPath != "" {
+		m.Attach(attachmentPath)
+	}
+
+	d := mail.NewDialer(c.Host, c.Port, c.AuthUsername, c.AuthPassword)
+
+	return d.DialAndSend(m)
+}
